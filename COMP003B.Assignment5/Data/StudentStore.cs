7b37a5c7769e7b8c4e1ef908f48a51a436f28664
gonzalez-7 <20agonzalezp@gmail.com>
2026-04-22 15:14:14 -0700

    Set up in memory Data Store - Created the Data Folder - Created StudentStore.cs

---
+++ COMP003B.Assignment5/Data/StudentStore.cs
@@ -0,0 +1,37 @@
+﻿using COMP003B.Assignment5.Models;
+
+namespace COMP003B.Assignment5.Data
+{
+	public static class StudentStore
+	{
+		public static List<Student> Students { get; set; } = new()
+		{
+			new Student
+			{
+				Id= 1,
+				FirstName = "Antonio",
+				LastName = "Gonzalez",
+				Age = 19,
+				Major = "Information Technology"
+			},
+
+			new Student
+			{
+				Id= 2,
+				FirstName = "Jonathan",
+				LastName = "Cruz",
+				Age= 28,
+				Major = "Business Administration"
+			},
+			new Student
+			{
+				Id= 3,
+				FirstName = "Alex",
+				LastName = "Lopez",
+				Age = 24,
+				Major = "Computer Science"
+
+			}
+		};
+	}
+}
