08164b178a6bfb2da3bd91131492bd6cdefe6f37
gonzalez-7 <20agonzalezp@gmail.com>
2026-04-22 14:55:16 -0700

    Created the Student.cs in the Model folder

---
+++ COMP003B.Assignment5/Models/Student.cs
@@ -0,0 +1,11 @@
+﻿namespace COMP003B.Assignment5.Models
+{
+	public class Student
+	{
+		public int Id { get; set; }
+		public string FirstName { get; set; } = string.Empty;
+		public string LastName { get; set; } = string.Empty;
+		public int Age { get; set; }
+		public string Major { get; set; } = string.Empty;
+	}
+}
